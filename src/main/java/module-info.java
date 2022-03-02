610688b188cb3d680b52095235e1449f3f90bb9b
Urjit Sharma <usharma9@my.bcit.ca>
2022-03-02 10:48:05 -0800

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ca.bcit.comp2522.termprojec.olu {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ca.bcit.comp2522.termprojec.olu to javafx.fxml;
+    exports ca.bcit.comp2522.termprojec.olu;
+}
