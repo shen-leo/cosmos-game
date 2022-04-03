1820c2ec1a1ad468b1a3b350f03c7233c8b415f7
shen-leo <shen.leo@outlook.com>
2022-04-02 22:18:47 -0700

    create LoginForm form and Class

---
+++ src/main/java/ca/bcit/comp2522/termprojec/olu/LoginForm.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp2522.termprojec.olu;
+
+import javax.swing.*;
+
+public class LoginForm extends JDialog {
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
+    private JButton btnLogin;
+    private JButton btnCancel;
+    private JPanel loginPanel;
+
+    public LoginForm(JFrame parent) {
+        super(parent);
+
+    }
+}
